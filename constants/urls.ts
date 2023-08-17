4ba7163f3cd402bedb207d763b930e79f9c2f96d
Aniket Chanana <aniketchanana@Velotios-MacBook-Air.local>
2023-08-17 21:37:37 +0530

    Minor fixes

--- constants/urls.ts
+++ constants/urls.ts
@@ -6,10 +6,24 @@ export enum authEndPoints {
   isValidSession = "/isValidSession",
 }
 
+export enum todoEndpoints {
+  root = "/todo",
+
+  createNewList = "/createNewList",
+  getUserTodoList = "/getUserTodoList",
+  updateTodoList = "/updateTodoList",
+  deleteTodoList = "/deleteTodoList",
+
+  createTodoItem = "/createTodoItem",
+  getUserTodoItem = "/getUserTodoItem",
+  updateTodoItem = "/updateTodoItem",
+  deleteTodoItem = "/deleteTodoItem",
+}
+
 const getFullEndPoints = <T>(endPoints): T => {
   const root = endPoints?.root || "";
   const values = {};
-  Object.entries(authEndPoints).forEach(([key, value]) => {
+  Object.entries(endPoints).forEach(([key, value]) => {
     if (key !== "root") values[key] = `${root}${value}`;
   });
   return values as T;
@@ -17,3 +31,5 @@ const getFullEndPoints = <T>(endPoints): T => {
 
 export const authApi =
   getFullEndPoints<Omit<typeof authEndPoints, "root">>(authEndPoints);
+export const todoApi =
+  getFullEndPoints<Omit<typeof todoEndpoints, "root">>(todoEndpoints);
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,25 @@
+import { todoApi } from "@/constants/urls";
+import { getApi } from "@/utils/api.utils";
 import { checkIfUserLoggedInInternalPage } from "@/utils/auth.utils";
 import { Box } from "@chakra-ui/react";
 
 import { GetServerSidePropsContext } from "next/types";
+import { useEffect } from "react";
 
 export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
   return await checkIfUserLoggedInInternalPage(ctx);
 };
 export default function Home() {
+  useEffect(() => {
+    (async () => {
+      try {
+        const res = await getApi(todoApi.getUserTodoList);
+
+        console.log(res);
+      } catch (e) {
+        console.log(e);
+      }
+    })();
+  }, []);
   return <Box marginTop={1}>Yeah... i am logged in!!!</Box>;
 }
